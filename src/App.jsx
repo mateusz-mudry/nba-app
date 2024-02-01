b48448a30b51d19ddeb3aa194d6546fce05acd8e
Mateusz Mudry <matmudry@gmail.com>
2024-02-01 22:28:34 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+import React from "react";
+import { Navigation } from "./components/NavigationBar/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation></Navigation>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const Navigation = () => {
+    return (
+        <header>
+            <div>NBA</div>
+            <div>News</div>
+            <div>Games</div>
+            <div>Standings</div>
+            <div>Teams</div>
+            <div>Players</div>
+        </header>
+    )
+}
