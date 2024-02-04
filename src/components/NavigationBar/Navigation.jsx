337628c70bc9fe1603d164abbaa3b8890f077489
Mateusz Mudry <matmudry@gmail.com>
2024-02-04 21:41:38 +0100

    Added media queries to NavigationBar

--- src/components/NavigationBar/Navigation.jsx
+++ src/components/NavigationBar/Navigation.jsx
@@ -7,30 +7,37 @@ import { NavBarStore } from "./Navigation.styles"
 import { LeaguePassButton } from "./Navigation.styles"
 import { NavBarStoreUl } from "./Navigation.styles"
 import { NavigationMenu } from "./Navigation.styles"
+import { MobileMenu } from "./Navigation.styles"
+import { MobileMenuIcon } from "./Navigation.styles"
 
 export const Navigation = () => {
     return (
         <NavigationBar>
             <NavigationBarDiv>
+                <MobileMenu>
+                    <MobileMenuIcon src={require('../../images/menu-icon.png')}></MobileMenuIcon>
+                </MobileMenu>
                 <div>
                 <NavigationBarLogo src={require('../../images/nba-logo.png')} />
                 </div>
                 <NavigationMenu>
-                    <NavigationUl>
+                    <NavigationUl>Menu</NavigationUl>
+                    {/* <NavigationUl>
                         <div>News</div>
                         <div>Games</div>
                         <div>Standings</div>
                         <div>Teams</div>
                         <div>Players</div>
-                    </NavigationUl>
-                    <NavBarStore>
+                    </NavigationUl> */}
+                    {/* <NavBarStore>
                         <LeaguePassButton>League Pass</LeaguePassButton>
                             <NavBarStoreUl>
                                 <div>Store</div>
                                 <div>Tickets</div>
                             </NavBarStoreUl>
                         <div>Sign In</div>
-                    </NavBarStore>
+                    </NavBarStore> */}
+                    <div>Sign In</div>
                 </NavigationMenu>
             </NavigationBarDiv>
         </NavigationBar>
