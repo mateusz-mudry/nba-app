c5411cb034f8c898079b637d3996a7211a02868f
Mateusz Mudry <matmudry@gmail.com>
2024-02-04 15:19:12 +0100

    Boxscore styled

--- src/components/BoxscoreBar/Boxscore.jsx
+++ src/components/BoxscoreBar/Boxscore.jsx
@@ -24,7 +24,7 @@ export const Boxscore = () => {
                         <ResultBox>
                             <ResultTeam>
                                 <ResultLogo src={require('../../images/team-logos/blazers.png')}></ResultLogo>
-                                <ResultTeamName>TMN</ResultTeamName>
+                                <ResultTeamName>por</ResultTeamName>
                             </ResultTeam>
                             <ResultScore>129</ResultScore>
                         </ResultBox>
@@ -33,9 +33,249 @@ export const Boxscore = () => {
                         <ResultBox>
                             <ResultTeam>
                                 <ResultLogo src={require('../../images/team-logos/rockets.png')}></ResultLogo>
-                                <ResultTeamName>TMN</ResultTeamName>
+                                <ResultTeamName>hou</ResultTeamName>
                             </ResultTeam>
                             <ResultScore>103</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/celtics.png')}></ResultLogo>
+                                <ResultTeamName>bos</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>99</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/hawks.png')}></ResultLogo>
+                                <ResultTeamName>atl</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>104</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/pistons.png')}></ResultLogo>
+                                <ResultTeamName>det</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>88</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/hornets.png')}></ResultLogo>
+                                <ResultTeamName>cha</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>98</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/bucks.png')}></ResultLogo>
+                                <ResultTeamName>mil</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>108</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/cavaliers.png')}></ResultLogo>
+                                <ResultTeamName>cle</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>107</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/lakers.png')}></ResultLogo>
+                                <ResultTeamName>lal</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>99</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/clippers.png')}></ResultLogo>
+                                <ResultTeamName>lac</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>95</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/jazz.png')}></ResultLogo>
+                                <ResultTeamName>uta</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>80</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/kings.png')}></ResultLogo>
+                                <ResultTeamName>sac</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>92</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/knicks.png')}></ResultLogo>
+                                <ResultTeamName>nyk</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>110</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/mavericks.png')}></ResultLogo>
+                                <ResultTeamName>dal</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>111</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/timberwolves.png')}></ResultLogo>
+                                <ResultTeamName>min</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>100</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/pelicans.png')}></ResultLogo>
+                                <ResultTeamName>nop</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>82</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/magic.png')}></ResultLogo>
+                                <ResultTeamName>orl</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>94</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/pacers.png')}></ResultLogo>
+                                <ResultTeamName>ind</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>98</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/wizards.png')}></ResultLogo>
+                                <ResultTeamName>was</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>93</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/raptors.png')}></ResultLogo>
+                                <ResultTeamName>tor</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>112</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                </BoxscoreBox>
+                <BoxscoreBox>
+                    <BoxscoreTop>
+                        <BoxscoreText>final</BoxscoreText>
+                        <BoxscoreText>lp</BoxscoreText>
+                    </BoxscoreTop>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/thunder.png')}></ResultLogo>
+                                <ResultTeamName>okc</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>132</ResultScore>
+                        </ResultBox>
+                    </ResultContainer>
+                    <ResultContainer>
+                        <ResultBox>
+                            <ResultTeam>
+                                <ResultLogo src={require('../../images/team-logos/spurs.png')}></ResultLogo>
+                                <ResultTeamName>sas</ResultTeamName>
+                            </ResultTeam>
+                            <ResultScore>121</ResultScore>
                         </ResultBox>
                     </ResultContainer>
                 </BoxscoreBox>
--- src/components/BoxscoreBar/Boxscore.styles.js
+++ src/components/BoxscoreBar/Boxscore.styles.js
@@ -5,45 +5,57 @@ export const BoxscoreComponent = styled.div`
   height: 90px;
   background-color: #ffff;
   box-shadow: -0px 2px 5px -2px rgba(191, 191, 194, 1);
-  padding: 0 40px;
   display: flex;
 `;
 
 export const GameScoreDiv = styled.div`
+  padding-left: 40px;
   height: 90px;
   width: 125px;
+  display: flex;
+`;
+
+export const BoxscoreBox = styled.div`
+  padding: 10px;
+  font-family: Arial, sans-serif;
+  text-transform: uppercase;
   border-left: 1px solid #e6e6e6;
   border-right: 1px solid #e6e6e6;
 `;
 
-export const BoxscoreBox = styled.div`
-  padding: 5px;
-`;
-
 export const BoxscoreTop = styled.div`
   height: 20px;
-  width: 115px;
+  width: 105px;
   display: flex;
   justify-content: space-between;
 `;
 
 export const BoxscoreText = styled.p`
   margin: 0;
-  text-transform: uppercase;
   font-size: 13px;
+  &:nth-child(2) {
+    color: #999999;
+  }
 `;
 
-export const ResultContainer = styled.div``;
+export const ResultContainer = styled.div`
+  padding-top: 2px;
+  padding-bottom: 2px;
+`;
 
 export const ResultBox = styled.div`
   height: 20px;
-  width: 115px;
+  width: 105px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  font-size: 13px;
+  font-weight: 600;
 `;
 
 export const ResultTeam = styled.div`
   display: flex;
+  align-items: center;
 `;
 
 export const ResultLogo = styled.img`
